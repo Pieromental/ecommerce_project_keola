c13aa81e9e36eebe606b091084e386e119dc34eb
Piero Alvaro Salazar Calle <54618161+Pieromental@users.noreply.github.com>
2024-08-07 04:11:23 -0500

    Enhance product entity and complete ecommerce structure
    - Enhanced `Producto` entity to include additional fields:
      - `nombre`: Product name.
      - `descripcion`: Product description.
      - `precio`: Product price.
      - `imagenUrl`: URL for the product image.
      - `stock`: Available stock for the product.

--- ecommerce_backend/src/main/java/com/keola/ecommerce_backend/models/Producto.java
+++ ecommerce_backend/src/main/java/com/keola/ecommerce_backend/models/Producto.java
@@ -11,7 +11,58 @@ public class Producto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
+    private String descripcion;
     private Double precio;
+    private String imagenUrl;
+    private Integer stock;
 
     // Getters y Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getImagenUrl() {
+        return imagenUrl;
+    }
+
+    public void setImagenUrl(String imagenUrl) {
+        this.imagenUrl = imagenUrl;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
 }
